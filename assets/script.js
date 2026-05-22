b8ad60297a7fd5e15af03f7a969f98099c58ec2a
ellicul <244015723+ellicul@users.noreply.github.com>
2026-05-22 11:49:24 +0100

    oureveryday died

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,9 @@
 async function getLua() {
     const appId = document.getElementById('appId').value;
-    const response = await fetch(`https://api.github.com/repos/SteamAutoCracks/ManifestHub/branches/${appId}`);
+    const response = await fetch(`https://api.github.com/repos/HW688/Steam-Inventory/branches/${appId}`);
     if (response.status === 200) {
         console.log("check working")
-        window.open(`https://codeload.github.com/SteamAutoCracks/ManifestHub/zip/refs/heads/${appId}`, '_blank');
+        window.open(`https://codeload.github.com/HW688/Steam-Inventory/zip/refs/heads/${appId}`, '_blank');
     } else {
         console.log("Falied to download manifest: " + response)
     }
